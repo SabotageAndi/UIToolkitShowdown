4beaa4bfa9472df7db1fe042b2bbb3781989648c
Andreas Willich <andreas.willich@techtalk.at>
2019-04-10 12:25:37 +0200

    add Avalonia UI examplec

---
+++ AvaloniaUI/AvaloniaUI/App.xaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaUI
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+   }
+}
